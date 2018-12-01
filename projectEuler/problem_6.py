d907269b14d29da2a33807e53e8748f59a67a45c
ShoTamuras-Git <solaskyfield@gmail.com>
2018-12-02 00:56:53 +0900

    problem6 commit!

---
+++ projectEuler/problem_6.py
@@ -0,0 +1,30 @@
+'''
+2018/12/01
+
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+'''
+
+def math(minN,maxN):
+    num_arr = []
+    numX = 0
+    numY = 0
+
+    for i in range(minN,maxN):
+        num_arr.append(i*i)
+        numY += i
+
+    numX = sum(num_arr)
+    numY = numY*numY
+
+    print(numY-numX)
+
+math(1,101)
