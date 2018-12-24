15726bb58454f4b42b7d2b68e39fac25c99c6598
ShoTamuras-Git <solaskyfield@gmail.com>
2018-12-24 15:46:49 +0900

    カレンダーは卑怯！

---
+++ projectEuler/Problem_19.py
@@ -0,0 +1,31 @@
+'''
+2018/12/24
+
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+'''
+from calendar import Calendar
+cl = Calendar()
+cld = []
+count = 0
+for y in range(1901,2001):
+    for m in range(1,13):
+        cld.append(cl.monthdayscalendar(y, m))
+
+for c in range(0,len(cld)):
+    if cld[c][0][6] == 1:
+        count += 1
+
+print(cld[1][0][0])
+print(count)
+print(len(cld))
