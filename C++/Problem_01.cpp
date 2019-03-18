50162f98e7e77ccfb9bc9a11d24225e09b1ac20f
ShoTamuras-Git <solaskyfield@gmail.com>
2019-03-19 02:17:46 +0900

    Initialization

--- C++/Problem_01.cpp
+++ C++/Problem_01.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main() {
-    int sum;
-    for(int i=1; i<1000; i++) {
+    int sum = 0;
+    int i = 1;
+    
+    for(i=1; i<1000; i++) {
         if (i % 3 == 0 || i % 5 == 0 ) {
             sum += i;
         }
